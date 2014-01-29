9a0ef9719febb24e2bd185bd501b5c76d1d352b5
Bradley Bossard <bradleybossard@gmail.com>
2014-01-29 21:37:42 +0000

    Added .bashrc to install process

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-ln -s `pwd`/.vimrc ~/.vimrc
 
+for filename in '.vimrc' '.bashrc'
+do
+  rm ~/$filename
+  ln -s `pwd`/$filename ~/$filename 
+done
+
+rm -rf ~/.vim/bundle
 #Install Vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
