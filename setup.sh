d489f40da75dbeb3e8a4cb1370660097aba154e9
Bradley Bossard <bradleybossard@gmail.com>
2014-08-27 12:27:37 -0700

    Testing out removing DOS line endings

--- setup.sh
+++ setup.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+## declare an array variable
+#declare -a arr=("element1" "element2" "element3")
+
+## now loop through the above array
+#for i in "${arr[@]}"
+#do
+#   echo "$i"
+   # or do whatever with individual element of the array
+#done
+
+# You can access them using echo "${arr[0]}", "${arr[1]}" also
+
+
+#declare -a arr=(".coolcommands.sh") # ".vimrc" ".bashrc" ".tmux.conf")
+#for filename in "${arr[@]}"
+
 for filename in '.coolcommands.sh' '.vimrc' '.bashrc' '.tmux.conf' '.gitconfig' '.ackrc' '.pylintrc'
 do
   unlink ~/$filename
   ln -s `pwd`/$filename ~/$filename 
 done
+
+
+#b='.gitconfig .ackrc .pylintrc svnlog.sh'
+#for filename in '.coolcommands.sh' '.vimrc'
+#'.bashrc' '.tmux.conf' '.gitconfig' '.ackrc' '.pylintrc' 'svnlog.sh'
 
 # Git credentials
 GIT_AUTHOR_NAME="Bradley Bossard"
