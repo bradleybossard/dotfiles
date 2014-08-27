6513f783c8c26eef33d1e874860fb1dad68c6527
Bradley Bossard <bradleybossard@gmail.com>
2014-08-27 13:04:44 -0700

    Added svnhead function, comments, and killed dead code.

--- .svnlog.sh
+++ .svnlog.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-escapeString()
-{
+escapeString() {
   newString=`echo $@ | sed 's/\:/\\\:/g' | sed 's/\-/\\\-/g'`
   echo $newString
 }
 
+# Get a list of my last commits for this directory and convert the timestamps to PST.
+# This function is useful for filling in my timesheet.
 svnlog () { 
   svn log | head -1000 | sed -n '/bbossard/,/-----$/ p' > svn.tmp
   dates_replace=$(cat svn.tmp | grep -v bbossard | sed 's/bbossard \| \(.*\) |/\1/')
@@ -25,4 +26,9 @@ svnlog () {
   done < svn.dates.tmp
   rm svn.dates.tmp
   less svn.tmp
+}
+
+# Copy the last commit message to clipboard.
+svnhead () {
+  svn log -r HEAD | pbcopy
 }
--- setup.sh
+++ setup.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-## declare an array variable
-#declare -a arr=("element1" "element2" "element3")
-
-## now loop through the above array
-#for i in "${arr[@]}"
-#do
-#   echo "$i"
-   # or do whatever with individual element of the array
-#done
-
-# You can access them using echo "${arr[0]}", "${arr[1]}" also
-
-
-#declare -a arr=(".coolcommands.sh") # ".vimrc" ".bashrc" ".tmux.conf")
-#for filename in "${arr[@]}"
 
 for filename in '.coolcommands.sh' '.vimrc' '.bashrc' \
                 '.tmux.conf' '.gitconfig' '.ackrc' '.pylintrc' '.svnlog.sh'
@@ -23,12 +8,7 @@ do
   ln -s `pwd`/$filename ~/$filename 
 done
 
-
-#b='.gitconfig .ackrc .pylintrc svnlog.sh'
-#for filename in '.coolcommands.sh' '.vimrc'
-#'.bashrc' '.tmux.conf' '.gitconfig' '.ackrc' '.pylintrc' 'svnlog.sh'
-
-# Git credentials
+# Setup my preferred Git configuration
 GIT_AUTHOR_NAME="Bradley Bossard"
 GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 git config --global user.name "$GIT_AUTHOR_NAME"
@@ -43,12 +23,8 @@ rm -rf ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone https://github.com/mattn/emmet-vim.git ~/.vim/bundle
 
-#for filename in $(find ~/.vim/); do flip -u $filename; done;
-
 #Install Vundle Bundles
 vim +BundleInstall +qall
-
-#for filename in $(find ~/.vim/); do flip -u $filename; done;
 
 echo
 echo 'Run command source ~/.bashrc'
