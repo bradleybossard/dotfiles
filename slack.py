6426f53c4058917ee7d153a158333e1f12fd2364
Bradley Bossard <bradleybossard@gmail.com>
2015-02-23 09:30:01 -0800

    Adding a repeating loop and translations to my Slack emoji tool.

--- slack.py
+++ slack.py
@@ -1,18 +1,31 @@
 import pyperclip
 
+translations = {}
 words = []
 
 f = open('slack_emoji.txt')
 for line in f:
   words.append(line.strip())
+f.close()
 
-input_text = raw_input("Type your slack: ")
+f = open('slack_translate.txt')
+for line in f:
+  (src, target) = line.strip().split('-')
+  translations[src] = target
+  print(translations[src])
+f.close()
 
-input_text += ' '
+while (True):
+  input_text = raw_input("Type your slack: ")
 
-for word in words:
-  input_text = input_text.replace(' ' + word + ' ', ' :' + word + ': ')
+  input_text += ' '
 
-print input_text
-pyperclip.copy(input_text)
-spam = pyperclip.paste()
+  for key in translations.keys():
+    input_text = input_text.replace(' ' + key + ' ', ' :' + translations[key] + ': ')
+
+  for word in words:
+    input_text = input_text.replace(' ' + word + ' ', ' :' + word + ': ')
+
+  print input_text
+  pyperclip.copy(input_text)
+  spam = pyperclip.paste()
