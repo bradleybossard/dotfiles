d2c8fc0fe9c7160a8c0696aada5c772532887f9d
Bradley Bossard <bradleybossard@gmail.com>
2015-06-04 17:44:06 +0000

    Removed stuff from setup.sh with is stored in my .gitconfig

--- setup.sh
+++ setup.sh
@@ -7,16 +7,6 @@ for file in $files; do
   ln -s `pwd`/$file ~/$file
 done
 
-# Setup my preferred Git configuration
-GIT_AUTHOR_NAME="Bradley Bossard"
-GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
-git config --global user.name "$GIT_AUTHOR_NAME"
-GIT_AUTHOR_EMAIL="bradleybossard@gmail.com"
-GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
-git config --global user.email "$GIT_AUTHOR_EMAIL"
-git config --global core.autocrlf input
-
-
 rm -rf ~/.vim/bundle
 #Install Vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
