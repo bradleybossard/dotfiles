09446bfdc703492378c0d85a9a1c79f787c5d3d9
Bradley Bossard <bradleybossard@gmail.com>
2016-06-16 23:26:21 +0000

    Adding a couple other must have tools

--- install_tools.sh
+++ install_tools.sh
@@ -1,5 +1,8 @@
+user=bradleybossard
+
 sudo apt-get update
 sudo apt-get -y remove ack
-sudo apt-get -y install git tmux vim ack-grep curl wget
+sudo apt-get -y install git tmux vim ack-grep curl wget grc tree fail2ban logwatch
 
 curl -sSL https://get.docker.com/ | sh
+sudo usermod -aG sudo $user
