98ab1ef5ab4f18cb03796431650195c03e18bfee
Bradley Bossard <bradleybossard@gmail.com>
2016-06-17 02:55:28 +0000

    Add bc

--- install_tools.sh
+++ install_tools.sh
@@ -4,7 +4,7 @@ user=bradleybossard
 
 sudo apt-get update
 sudo apt-get -y remove ack
-sudo apt-get -y install git tmux vim ack-grep curl wget grc tree fail2ban logwatch
+sudo apt-get -y install git tmux vim ack-grep curl wget grc bc tree fail2ban logwatch
 
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG sudo $user
