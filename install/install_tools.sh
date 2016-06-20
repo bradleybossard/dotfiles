0d94c57580de8394c31418a793c3ed0b92e07d6e
Bradley Bossard <bradleybossard@gmail.com>
2016-06-20 01:06:00 +0000

    Add R to list of installed packages

--- install/install_tools.sh
+++ install/install_tools.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 sudo apt-get -y remove ack
 sudo apt-get -y install git tmux vim ack-grep curl wget grc bc tree fail2ban \
                         logwatch flex bison pandoc context golang-go haskell-platform \
-                        virtualenv
+                        virtualenv r-base-core
 
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG sudo $user
