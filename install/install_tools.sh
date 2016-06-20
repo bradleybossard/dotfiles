545b2913c3dc9013a8f2678211be5e090d86b4c3
Bradley Bossard <bradleybossard@gmail.com>
2016-06-20 00:31:22 +0000

    Adding virutalenv

--- install/install_tools.sh
+++ install/install_tools.sh
@@ -7,7 +7,8 @@ user=bradleybossard
 sudo apt-get update
 sudo apt-get -y remove ack
 sudo apt-get -y install git tmux vim ack-grep curl wget grc bc tree fail2ban \
-                        logwatch flex bison pandoc context golang-go haskell-platform
+                        logwatch flex bison pandoc context golang-go haskell-platform \
+                        virtualenv
 
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG sudo $user
