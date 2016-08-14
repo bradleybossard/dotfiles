87df04139760dd99cc0ee62fcc7f1e4c2dc8a937
Bradley Bossard <bradleybossard@gmail.com>
2016-08-14 18:20:28 +0000

    Adding nginx

--- install/install_tools.sh
+++ install/install_tools.sh
@@ -10,7 +10,7 @@ sudo apt-get -y remove ack
 sudo apt-get -y install git tmux vim ack-grep curl wget grc bc tree fail2ban \
                         logwatch flex bison pandoc context golang-go haskell-platform \
                         virtualenv r-base-core silversearcher-ag imagemagick jq hugo \
-                        xmlstarlet
+                        xmlstarlet nginx
 
 go get -u github.com/aykamko/tag/...
 go install github.com/aykamko/tag
