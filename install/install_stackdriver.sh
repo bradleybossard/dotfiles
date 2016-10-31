4ca394a6d40b5d6a384a4eb43709b92813f112bc
Bradley Bossard <bradleybossard@gmail.com>
2016-10-31 15:14:29 +0000

    Install nginx plugin

--- install/install_stackdriver.sh
+++ install/install_stackdriver.sh
@@ -3,3 +3,19 @@ sudo bash stack-install.sh --write-gcm
 
 curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
 sudo bash install-logging-agent.sh
+
+rm install-logging-agent.sh
+rm stack-install.sh
+
+pushd .
+cd /etc/nginx/conf.d/
+sudo curl -O https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/nginx/conf.d/status.conf
+popd
+sudo service nginx restart
+
+pushd .
+cd /opt/stackdriver/collectd/etc/collectd.d/;
+sudo curl -O https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/collectd.d/nginx.conf
+popd
+sudo service stackdriver-agent restart
+
