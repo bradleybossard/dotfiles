84a53594b66592330dd02cc4de01e327368010f5
Bradley Bossard <bradleybossard@gmail.com>
2016-10-31 14:50:41 +0000

    Add install stack driver install commands

---
+++ install/install_stackdriver.sh
@@ -0,0 +1,5 @@
+curl -sSO https://repo.stackdriver.com/stack-install.sh
+sudo bash stack-install.sh --write-gcm
+
+curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+sudo bash install-logging-agent.sh
