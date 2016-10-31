ba199d4c92839ae161b5e3c2188a75cc49db946a
Bradley Bossard <bradleybossard@gmail.com>
2016-10-31 15:17:21 +0000

    Add docs

--- install/install_stackdriver.sh
+++ install/install_stackdriver.sh
@@ -1,12 +1,16 @@
+# Install stackdriver agent
 curl -sSO https://repo.stackdriver.com/stack-install.sh
 sudo bash stack-install.sh --write-gcm
 
+# Install logging agent
 curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
 sudo bash install-logging-agent.sh
 
+# Cleanup scripts
 rm install-logging-agent.sh
 rm stack-install.sh
 
+# Install nginx agents
 pushd .
 cd /etc/nginx/conf.d/
 sudo curl -O https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/nginx/conf.d/status.conf
