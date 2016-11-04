f38086d5508db2cc462a9ff9969806d1a5229232
Bradley Bossard <bradleybossard@gmail.com>
2016-11-04 23:45:47 +0000

    Alphabetized packages

--- install/install_node.sh
+++ install/install_node.sh
@@ -5,31 +5,30 @@ sudo sh -c 'curl -sL https://deb.nodesource.com/setup_7.x | bash -'
 sudo apt-get install -y nodejs pkg-config libcairo2-dev \
 libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
 
-sudo npm install -g webpack \
-               webpack-dev-server \
-               yarn \
-               caniuse-cmd \
-               st \
-               wintersmith \
+sudo npm install -g
+               angular-cli \
+               babelify \
+               browserify \
                bower \
+               caniuse-cmd \
+               generator-angular-fullstack \
+               grunt \
                gulp \
                gulp-cli \
-               yo \
-               grunt \
-               node-gyp \
-               node-inspector \
                jspm \
                live-server \
+               node-gyp \
+               node-inspector \
+               nodemon \
+               protractor \
+               quget \
+               st \
+               svgo
                typescript \
                typings \
-               nodemon \
-               quget \
-               angular-cli \
-               protractor \
-               browserify \
                watchify \
-               babelify \
-               generator-angular-fullstack \
-               svgo
-
-#              nativescript \
+               webpack \
+               webpack-dev-server \
+               wintersmith \
+               yarn \
+               yo
