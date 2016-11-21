535b3a1531e1cc252633ef7a849563761b48e194
Bradley Bossard <bradleybossard@gmail.com>
2016-11-21 15:47:53 +0000

    Added docstring for source

--- install/install_swift.sh
+++ install/install_swift.sh
@@ -1,9 +1,9 @@
+# http://dev.iachieved.it/iachievedit/swift-3-0-for-ubuntu-16-04-xenial-xerus/
+
 wget -qO- http://dev.iachieved.it/iachievedit.gpg.key | sudo apt-key add -
 
 echo "deb http://iachievedit-repos.s3.amazonaws.com/ xenial main" | sudo tee --append /etc/apt/sources.list
 
 sudo apt-get update
 
-sudo apt-get install swift-3.0
-
-export PATH=/opt/swift/swift-3.0/usr/bin:$PATH
+sudo apt-get install --yes swift-3.0
