87821d182f65ebf69db1c14124e977ef4aa83feb
Bradley Bossard <bradleybossard@gmail.com>
2017-02-06 16:43:32 -0800

    Adding an OSX install file

--- install/install_osx.sh
+++ install/install_osx.sh
@@ -1,2 +1,27 @@
+brew unlink tag  # unlink default osx tag
+
+
+brew install /
+  bash-completion /
+  freetype /
+  go /
+  jq /
+  imagemagick /
+  jpeg /
+  libevent /
+  libpng /
+  libtiff /
+  libtool /
+  node /
+  oniguruma /
+  pcre /
+  openssl /
+  tag-ag /
+  the_silver_searcher /
+  tmux /
+  tree /
+  wget /
+  xz
+
 brew tap aykamko/tag-ag
 brew install tag-ag
