62f816428e9756b0a1ec8359e61ab8844f1a33d2
Bradley Bossard <bradleybossard@gmail.com>
2017-02-11 09:34:39 -0800

    Change slashes to backslashes

--- install/install_osx.sh
+++ install/install_osx.sh
@@ -1,26 +1,26 @@
 brew unlink tag  # unlink default osx tag
 
-brew install /
-  bash-completion /
-  freetype /
-  go /
-  jq /
-  imagemagick /
-  jpeg /
-  libevent /
-  libpng /
-  libtiff /
-  libtool /
-  node /
-  tag-ag /
-  the_silver_searcher /
-  tmux /
-  tree /
+brew install \
+  bash-completion \
+  freetype \
+  go \
+  jq \
+  imagemagick \
+  jpeg \
+  libevent \
+  libpng \
+  libtiff \
+  libtool \
+  node \
+  tag-ag \
+  the_silver_searcher \
+  tmux \
+  tree \
   wget
 
-# oniguruma /
-# pcre /
-# openssl /
+# oniguruma \
+# pcre \
+# openssl \
 # xz
 
 brew tap aykamko/tag-ag
