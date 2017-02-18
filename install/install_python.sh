8969c478c7cefeb17e7c4b18cabffc8b8f4f6d08
Bradley Bossard <bradleybossard@gmail.com>
2017-02-18 08:20:15 -0800

    Update python install to support Darwin

--- install/install_python.sh
+++ install/install_python.sh
@@ -3,13 +3,19 @@ source ~/.bashrc
 # Follow up, do I need to install pip globally? Or even pylint, pyflakes, etc.
 # Maybe they belong in postmkvirtualenv
 # https://virtualenvwrapper.readthedocs.io/en/latest/scripts.html#scripts-postmkvirtualenv
-sudo apt-get -y install python-pip
 
-sudo apt-get -y install python-pip virtualenv virtualenvwrapper pylint pyflakes cookiecutter
+if [[ $OSTYPE == *"darwin"* ]]; then
+  pip3 install virtualenv virtualenvwrapper
+  #pip3 install pylint pyflakes cookiecutter
+else
+  # TODO: Revisit this, test OS... not sure to install pylint, etc globally
+  sudo apt-get -y install python-pip virtualenv virtualenvwrapper pylint pyflakes cookiecutter
+fi
+
 mkdir -p $WORKON_HOME
 mkdir -p $PROJECT_HOME
 
-sudo pip install cookiecutter --upgrade
+#sudo pip install cookiecutter --upgrade
 
 # install pyenv
 #curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
