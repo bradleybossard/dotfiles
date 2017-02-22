094e830c26261101fd03eb1bb14ddb72650b921a
Bradley Bossard <bradleybossard@gmail.com>
2017-02-21 20:43:15 -0800

    Add instructions for installing CUDA

--- install/install_osx.sh
+++ install/install_osx.sh
@@ -6,8 +6,10 @@ brew unlink tag  # unlink default osx tag
 
 brew install \
   bash-completion \
+  cowsay \
   freetype \
   go \
+  fortune \ 
   imagemagick \
   jpeg \
   jq \
@@ -22,6 +24,13 @@ brew install \
   tmux \
   tree \
   wget
+
+# Install CUDA https://gist.github.com/Mistobaan/dd32287eeb6859c6668d 
+brew update
+brew install coreutils swig
+brew cask install cuda
+brew cask info cuda
+
 
 # oniguruma \
 # pcre \
