39a52b105036a57a86f2c34a48b32faeb0a89bf7
Bradley Bossard <bradley.bossard@moovel.com>
2017-03-29 12:59:39 -0700

    Move unlink tag closer to where tag gets installed

--- install/install_osx.sh
+++ install/install_osx.sh
@@ -3,8 +3,6 @@ if [[ $(which brew | wc -c) -eq 0 ]]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew unlink tag  # unlink default osx tag
-
 brew install \
   bash-completion \
   cowsay \
@@ -47,6 +45,9 @@ nvm install node; nvm use node
 
 pip intall virtualenv
 pip intall virtualenvwrapper
+
+
+brew unlink tag  # unlink default osx tag
 
 brew tap aykamko/tag-ag
 brew install tag-ag
