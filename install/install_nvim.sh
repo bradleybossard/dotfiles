f2fc6a87c64267714353e75b99a7721c03d05122
Bradley Bossard <bradleybossard@gmail.com>
2017-06-02 16:56:26 +0000

    Add script for install neovim on ubuntu

---
+++ install/install_nvim.sh
@@ -0,0 +1,12 @@
+sudo apt-get install --yes software-properties-common
+sudo apt-get install --yes python-software-properties
+sudo add-apt-repository --yes ppa:neovim-ppa/stable
+sudo apt-get update --fix-missing
+sudo apt-get install --yes neovim
+sudo apt-get install --yes python-dev python-pip python3-dev python3-pip
+#sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+#sudo update-alternatives --config vi
+#sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+#sudo update-alternatives --config vim
+#sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+#sudo update-alternatives --config editor
