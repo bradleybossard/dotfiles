72646592cfdef679ee4001ffb60b63d8a7841f8d
Bradley Bossard <bradleybossard@gmail.com>
2017-06-11 16:01:52 +0000

    Add url for BASH strict mode

--- install/install_tools.sh
+++ install/install_tools.sh
@@ -2,6 +2,7 @@
 # http://www.codelitt.com/blog/my-first-10-minutes-on-a-server-primer-for-securing-ubuntu/
 # https://plusbryan.com/my-first-5-minutes-on-a-server-or-essential-security-for-linux-servers
 # https://www.inversoft.com/guides/2016-guide-to-user-data-security
+# BASH strict mode - http://redsymbol.net/articles/unofficial-bash-strict-mode/
 
 ## Youtube Downloader
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
