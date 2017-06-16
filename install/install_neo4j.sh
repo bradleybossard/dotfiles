eed7f6b0db3d46f3d7ac676f6c99e9c44121afbb
Bradley Bossard <bradleybossard@gmail.com>
2017-06-16 22:29:23 +0000

    Script to install neo4j

---
+++ install/install_neo4j.sh
@@ -0,0 +1,7 @@
+sudo wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
+sudo apt-get update --fix-missing
+
+sudo apt-get install --yes neo4j
+
+sudo service neo4j start
