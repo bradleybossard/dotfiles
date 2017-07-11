ebe89fbb794ad458b28ed06a07c602527877b1db
Bradley Bossard <bradleybossard@gmail.com>
2017-07-11 17:57:21 +0000

    Add notes to exposing neo4j to web

--- install/install_neo4j.sh
+++ install/install_neo4j.sh
@@ -5,3 +5,10 @@ sudo apt-get update --fix-missing
 sudo apt-get install --yes neo4j
 
 sudo service neo4j start
+
+# In order to access the server from non-local host, uncomment the following line in
+# /etc/neo4j/neo4j.conf
+#
+# dbms.connectors.default_listen_address=0.0.0.0
+#
+# Access the browser on port :7474
