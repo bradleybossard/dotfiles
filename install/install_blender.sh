3443f5b47e070131832b9bf9d7a7aa3989192014
Bradley Bossard <bradleybossard@gmail.com>
2017-10-08 18:19:41 -0700

    Adding --classic flag for Blender

--- install/install_blender.sh
+++ install/install_blender.sh
@@ -1,6 +1,1 @@
-#sudo add-apt-repository ppa:thomas-schiex/blender
-#sudo apt-get update
-#sudo apt-get install --yes blender
-#sudo adduser $USER video
-
-sudo snap install blender
+sudo snap install --classic blender
