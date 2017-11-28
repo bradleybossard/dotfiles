20d5fc516c464bd7da627be07c4888109045bdee
Bradley Bossard <bradleybossard@gmail.com>
2017-11-27 16:09:28 -0800

    Script for installing docker-compose

---
+++ install/install_docker_compose.sh
@@ -0,0 +1,7 @@
+# GET the latest release using the Github API
+VERSION=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq '.name' | sed 's/"//g')
+echo $VERSION
+
+sudo curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
