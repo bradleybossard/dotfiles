20b96df8663c94d15c9749a667ed83581c59a36c
Bradley Bossard <bradleybossard@gmail.com>
2018-02-07 09:41:39 -0800

    Adding verified instructions for install NVIDIA Linux driver

---
+++ install/install_nvidia_driver.sh
@@ -0,0 +1,32 @@
+# 1. Download the driver from the nvidia website
+
+# 2. If Nouveau driver needs to be disabled, follow these steps. 
+sudo vi /etc/modprobe.d/blacklist-nouveau.conf
+# Add the following lines
+blacklist nouveau
+options nouveau modeset=0
+
+# Regenerate the kernel initramfs
+sudo update-initramfs -u
+
+# Reboot the machine
+sudo reboot
+
+# 3. Install the NVIDIA driver
+# Hit Ctrl + Alt + F1
+# Kill X Server
+sudo service lightdm stop
+
+sudo init 3
+
+# cd to directory where driver installer is and change permissions to executable
+cd ~/Downloads
+chmod +x ./NVIDIA-Linux-*
+
+# Run the installer
+sudo ./NVIDIA-Linux-whatever-driver-version.sh
+
+# After installer is finished, reboot
+sudo reboot
+
+
