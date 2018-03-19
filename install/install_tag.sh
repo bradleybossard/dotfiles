3e758db10295b528b98ed0ead9c9a127ede33563
Bradley Bossard <bradleybossard@gmail.com>
2018-03-19 11:38:02 -0700

    Linux/OSX instructions for installing tag

---
+++ install/install_tag.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+
+# tag - instantly jump to your ag matches
+if [[ $OSTYPE == *"linux"* ]]; then
+  # install silver searcher
+  sudo apt-get update --fix-missing
+  sudo apt-get install --yes silversearcher-ag
+
+  # install go
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  "$DIR/install_go.sh"
+
+  go get -u github.com/aykamko/tag/...
+  go install github.com/aykamko/tag
+fi
+
+if [[ $OSTYPE == *"darwin"* ]]; then
+  brew unlink tag  # unlink default osx tag
+  brew tap aykamko/tag-ag
+  brew install tag-ag
+fi
