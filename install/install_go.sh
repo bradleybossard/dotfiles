381b3abb93a9e0ba1e198382803276c5d28e7919
Bradley Bossard <bradleybossard@gmail.com>
2018-04-28 12:52:28 -0700

    Fix errors to go install script

--- install/install_go.sh
+++ install/install_go.sh
@@ -1,8 +1,8 @@
-#/bin/sh
+#!/bin/bash
 if [[ $OSTYPE == *"linux"* ]]; then
   sudo snap install --classic go
 fi
 
-if [[ $OSTYPE == *"linux"* ]]; then
+if [[ $OSTYPE == *"darwin"* ]]; then
   brew install go
 fi
