01580597c7b16b917ab773dffc21ef41bb6a298f
Bradley Bossard <bradleybossard@gmail.com>
2018-05-14 07:58:19 -0700

    Updating with some shellchecking

--- install/install_node.sh
+++ install/install_node.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+set -euo pipefail
 # install nvm then latest node
-if [[ $(ls -a $HOME | grep .nvm | wc -c) -eq 0 ]]; then
+if [ $(ls -a "$HOME" | grep .nvm | wc -c) -eq 0 ]; then
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 fi
 
 export NVM_DIR="$HOME/.nvm"
+# shellcheck source=~/.nvm/nvm.sh
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-cp ./nvm-default-packages $NVM_DIR/default-packages
+cp ./nvm-default-packages "$NVM_DIR/default-packages"
 # nvm install node;
 nvm install v8.9.1;
 # nvm use node
