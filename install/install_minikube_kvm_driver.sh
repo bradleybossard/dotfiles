e96c0b2f73805d198cfa5c378fc7ef782a99ce83
Bradley Bossard <bradleybossard@gmail.com>
2018-05-16 16:19:32 -0500

    Install script for minikube kvm driver

---
+++ install/install_minikube_kvm_driver.sh
@@ -0,0 +1,5 @@
+# installing kvm driver for minikube
+
+sudo apt-get install --yes libvirt-bin
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 && chmod +x docker-machine-driver-kvm2 && sudo mv docker-machine-driver-kvm2 /usr/local/bin/
