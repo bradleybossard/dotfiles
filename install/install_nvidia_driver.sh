1a613d97b59d230796a41dec9b03c115eb9fe75e
Bradley Bossard <bradleybossard@gmail.com>
2018-05-17 07:11:25 -0700

    Updated install instructions for Ubuntu 18.04

--- install/install_nvidia_driver.sh
+++ install/install_nvidia_driver.sh
@@ -6,12 +6,15 @@
 # http://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#ixzz4rQODN0jy
 
 # 1. Download the driver from the nvidia website
+# http://www.nvidia.com/Download/index.aspx
 
 # 2. If Nouveau driver needs to be disabled, follow these steps. 
-sudo vi /etc/modprobe.d/blacklist-nouveau.conf
+# sudo vi /etc/modprobe.d/blacklist-nouveau.conf
 # Add the following lines
-blacklist nouveau
-options nouveau modeset=0
+# blacklist nouveau
+# options nouveau modeset=0
+sudo sh -c 'echo blacklist nouveau > /etc/modprobe.d/blacklist-nouveau.conf'
+sudo sh -c 'echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nouveau.conf'
 
 # Regenerate the kernel initramfs
 sudo update-initramfs -u
@@ -19,12 +22,14 @@ sudo update-initramfs -u
 # Reboot the machine
 sudo reboot
 
+# NOTE: On 2018-05-17, while install the NVIDIA driver on Ubuntu 18.04, the follow step 3.
+# was not needed.  Simply running the NVIDIA install shell script under X was able to
+# successfully install the driver
 # 3. Install the NVIDIA driver
 # Hit Ctrl + Alt + F1
 # Kill X Server
-sudo service lightdm stop
-
-sudo init 3
+#sudo service lightdm stop
+#sudo init 3
 
 # cd to directory where driver installer is and change permissions to executable
 cd ~/Downloads
@@ -36,4 +41,5 @@ sudo ./NVIDIA-Linux-whatever-driver-version.sh
 # After installer is finished, reboot
 sudo reboot
 
-
+# Verify driver is working
+nvidia-smi
