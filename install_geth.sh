666e90fc6f63c3d3624527e9274227145104520b
Bradley Bossard <bradleybossard@gmail.com>
2018-06-30 20:37:03 -0500

    Add geth install

---
+++ install_geth.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository --yes ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install --yes ethereum
+
+curl -O genesis.json https://gist.githubusercontent.com/jashmenn/f1336e4e69b8007bbce883ace96af3ea/raw/7ca068ac635ce77cdd93f1a1f06c37bef7dd90bf/genesis.json
+
