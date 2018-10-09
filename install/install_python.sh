ccd6294b71025c465feb4577dc10ddbd1673e803
Bradley Bossard <bradleybossard@gmail.com>
2018-10-08 20:49:52 -0700

    Updates to install python via pyenv

--- install/install_python.sh
+++ install/install_python.sh
@@ -1,5 +1,5 @@
 # Sourcing .bashrc for $WORKON_HOME env variable
-source ~/.bashrc
+# source ~/.bashrc
 # Follow up, do I need to install pip globally? Or even pylint, pyflakes, etc.
 # Maybe they belong in postmkvirtualenv
 # https://virtualenvwrapper.readthedocs.io/en/latest/scripts.html#scripts-postmkvirtualenv
@@ -12,7 +12,7 @@ source ~/.bashrc
   #pip3 install pylint pyflakes cookiecutter
 #else
   # TODO: Revisit this, test OS... not sure to install pylint, etc globally
-  sudo apt-get -y install python-pip virtualenv virtualenvwrapper pylint pyflakes cookiecutter
+  #sudo apt-get -y install python-pip virtualenv virtualenvwrapper pylint pyflakes cookiecutter
 #fi
 
 # Moved these to .bashrc
@@ -20,9 +20,3 @@ source ~/.bashrc
 #mkdir -p $PROJECT_HOME
 
 #sudo pip install cookiecutter --upgrade
-
-# install pyenv
-#curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-
-# install pyenv-virtualenv
-#git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
