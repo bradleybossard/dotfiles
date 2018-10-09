d2bb2e8acf0dad7b5539bd2266c81a12a17c059c
Bradley Bossard <bradleybossard@gmail.com>
2018-10-08 21:04:03 -0700

    Updated instructions for installing CUDA on Ubuntu

--- install/install_cuda.sh
+++ install/install_cuda.sh
@@ -1,12 +1,32 @@
-#/bin/sh
+#!/bin/bash
 
 if [[ $OSTYPE == *"linux"* ]]; then
   # TODO: Revisit these instructions, this url is no longer live
   # Update version to latest
+  CUDA_VERSION=10.0
   # CUDA_FILE=cuda_9.1.85_387.26_linux
+  CUDA_FILE=cuda_10.0.130_410.48_linux
   # wget https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/$CUDA_FILE
-  # chmod +x $CUDA_FILE
-  # sudo ./$CUDA_FILE
+  # wget https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/$CUDA_FILE
+  pushd .
+  cd ~/Downloads
+  wget https://developer.nvidia.com/compute/cuda/$CUDA_VERSION/Prod/local_installers/$CUDA_FILE
+  chmod +x $CUDA_FILE
+  sudo ./$CUDA_FILE
+  # TODO: Try the command below to test full NVIDIA driver / CUDA install
+  # sudo $CUDA_FILE --silent --driver --toolkit --samples
+  # TODO: Haven't tried command below, but don't think the
+  # flags are correct for the CUDA installer
+  #sudo $CUDA_FILE \
+  # --ui=none \
+  # --no-questions \
+  # --accept-license \
+  # --disable-nouveau \
+  # --no-cc-version-check \
+  # --dkms
+
+  nvcc --version  # To test CUDA compiler was installed correctly.
+  popd
 fi
 
 
