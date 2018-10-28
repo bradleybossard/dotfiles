4f8f2e10304c2ee4dc39a546a90d465aaa2524a2
Bradley Bossard <bradleybossard@gmail.com>
2018-10-28 11:07:51 -0700

    Initial version of ROS install script

---
+++ install/install_ros.sh
@@ -0,0 +1,14 @@
+# add to sources
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+# add keys
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+# update
+sudo apt update --fix-missing
+
+sudo apt install --yes ros-melodic-desktop-full
+
+# update rosdep
+sudo rosdep init
+rosdep update
