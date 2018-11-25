0a9c8673cbc3eb3723e337201670d559c5717e48
Bradley Bossard <bradleybossard@gmail.com>
2018-11-24 17:04:20 -0800

    Scripts for non-standard Kali tools

---
+++ install/install_kali_tools.sh
@@ -0,0 +1,20 @@
+mkdir -p ~/src
+cd ~/src
+
+# dnscan is a python wordlist-based DNS subdomain scanner.
+git clone https://github.com/rbsec/dnscan.git
+
+# A DNS meta-query spider that enumerates DNS records, and subdomains.
+git clone https://github.com/TheRook/subbrute.git
+
+# Analysis of DNS records to find popular trends
+git clone https://github.com/bitquark/dnspop.git
+
+# Web path scanner
+git clone https://github.com/maurosoria/dirsearch.git
+
+# Tor
+apt install tor
+
+# Router Hunter
+git clone https://github.com/Exploit-install/Routerhunter-2.0.git
