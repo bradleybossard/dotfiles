1a7182661b7e1760aa6d3fd5da8d02df7af485dd
Bradley Bossard <bradleybossard@gmail.com>
2018-12-03 14:11:15 -0800

    Add logic to create renamed copy

--- scripts/optimize_pdfs.py
+++ scripts/optimize_pdfs.py
@@ -1,11 +1,20 @@
 import os
 import glob
+from shutil import copyfile
 
 # dir = '/Users/bradleybossard/test/shrink-pdf'
+dir = '.'
 pdf_paths = dir + '/*.pdf'
+target_path = dir + '/renamed/'
+
+
+if not os.path.exists(target_path):
+    os.makedirs(target_path)
 
 for name in glob.glob(pdf_paths):
     basename = os.path.basename(name)
     parts = basename.split('-')
     title = parts[1].replace('_', ' -')
     print(title)
+    target_name = target_path + title + '.pdf'
+    copyfile(name, target_name)
