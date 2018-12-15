e56dac16bc0c60bd61770b95ba45553cd8baa751
Bradley Bossard <bradleybossard@gmail.com>
2018-12-15 12:24:16 -0600

    Adding updating install instructions for Bionic Beaver

--- install/install_postgresql.sh
+++ install/install_postgresql.sh
@@ -1,3 +1,6 @@
 if [[ $OSTYPE == *"linux"* ]]; then
   sudo apt-get install --yes postgresql postgresql-client postgresql-client-common
+  # TODO: According to this tutorial, all that is needed is the following, need to verify
+  # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-18-04
+  # sudo apt install postgresql postgresql-contrib
 fi
