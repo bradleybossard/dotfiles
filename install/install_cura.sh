790444a818a9133e46daac36d4441c84e5049b44
Bradley Bossard <bradleybossard@gmail.com>
2018-12-31 15:42:24 -0800

    Remove PPA instructions

--- install/install_cura.sh
+++ install/install_cura.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo add-apt-repository ppa:thopiekar/cura
-sudo apt-get update
-sudo apt-get install --yes cura
+# Ignore the PPA for now, best way seems to be downloading from site
+# In the future, check for a snap package
+# sudo add-apt-repository ppa:thopiekar/cura-master
+# sudo apt-get update
+# sudo apt-get install --yes cura
