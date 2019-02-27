4a1cf2356c08da57d3180ab8eb6075be2ed55ad3
Bradley Bossard <bradleybossard@gmail.com>
2019-02-26 16:06:27 -0800

    Adding devd to go tooling

--- install/install_go_tools.sh
+++ install/install_go_tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-go get github.com/Rican7/define     # CLI dictionary written in go
-go get github.com/aykamko/tag       # instantly jump to your ag matches
-go get github.com/github/hub        # hub - win at github
-go get -u github.com/tomnomnom/gron # gron - flatten json
-go get -u github.com/maxzender/jv   # jv - interactive json viewer
+go get github.com/Rican7/define            # CLI dictionary written in go
+go get github.com/aykamko/tag              # instantly jump to your ag matches
+go get github.com/github/hub               # hub - win at github
+go get -u github.com/tomnomnom/gron        # gron - flatten json
+go get -u github.com/maxzender/jv          # jv - interactive json viewer
+go get github.com/cortesi/devd/cmd/devd    # development web server
