9e3c557382b9a4f9a52c888f1aa0c67fe446f263
Bradley Bossard <bradleybossard@gmail.com>
2019-03-05 17:31:51 -0600

    Temp file for snap packages to check out

---
+++ install/install_snap_packages.sh
@@ -0,0 +1,7 @@
+sudo snap install opentoonz
+sudo snap install obs-studio
+sudo snap install shotcut --classic
+sudo snap install wormhole
+sudo snap install kata-containers --classic
+sudo snap install sickgear
+sudo snap install ultimate-media-downloader
