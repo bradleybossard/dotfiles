83a444d260889f3bda859fd0f23bcd9d0d2681c6
Bradley Bossard <bradleybossard@gmail.com>
2019-03-05 17:15:33 -0600

    More setup steps for running elasticsearch service

--- install/install_elastic_search.sh
+++ install/install_elastic_search.sh
@@ -1,3 +1,19 @@
 DEB_FILE=elasticsearch-6.6.1.deb
 wget https://artifacts.elastic.co/downloads/elasticsearch/$DEB_FILE
-#sudo apt install $DEB_FILE
+
+# Enable elastic search to start on boot
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable elasticsearch.service
+
+# Start the service
+sudo systemctl start elasticsearch.service
+# sudo systemctl stop elasticsearch.service
+
+# Verify the system is working
+curl -X GET "localhost:9200/"
+
+# Tail the journal output of the service
+# sudo journalctl --unit elasticsearch --follow
+
+# Guide to file layout, etc
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/deb.html
