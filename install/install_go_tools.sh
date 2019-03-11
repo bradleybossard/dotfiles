92e7e3334e64d04c73a718333c66cd526dfcadca
Bradley Bossard <bradleybossard@gmail.com>
2019-03-11 16:50:23 -0500

    Add script for install dep

--- install/install_go_tools.sh
+++ install/install_go_tools.sh
@@ -6,3 +6,6 @@ go get -u github.com/tomnomnom/gron        # gron - flatten json
 go get -u github.com/maxzender/jv          # jv - interactive json viewer
 go get github.com/cortesi/devd/cmd/devd    # development web server
 go get github.com/cortesi/modd/cmd/modd    # filesystem watcher
+
+# install dep - go package management
+curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
