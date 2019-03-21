055004e2edc8391b6ef1d9033dee205c51716d2d
Bradley Bossard <bradleybossard@gmail.com>
2019-03-21 14:11:26 -0700

    Adding Inkscpae

--- install/install_flatpak_apps.sh
+++ install/install_flatpak_apps.sh
@@ -1,2 +1,3 @@
+sudo flatpak install --system --assumeyes flathub org.inkscape.Inkscape
 sudo flatpak install --system --assumeyes flathub com.visualstudio.code
 sudo flatpak install --system --assumeyes flathub org.zealdocs.Zeal
