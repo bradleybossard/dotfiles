6560820cbe5525c701f3afce2d18fa9fb9e1626a
Bradley Bossard <bradleybossard@gmail.com>
2019-03-26 14:13:22 -0500

    Adding some links for further bcc play

--- install/install_bcc.sh
+++ install/install_bcc.sh
@@ -5,3 +5,10 @@ sudo apt-get update
 sudo apt-get install --yes bcc-tools libbcc-examples linux-headers-$(uname -r)
 
 # Tools are located in /usr/share/bcc/tools
+
+
+# Further reading
+# [Virtual filesystems: Why we need them and how they work](https://opensource.com/article/19/3/virtual-filesystems-linux?utm_campaign=intrel)
+# [bcc/tutorial.md](https://github.com/iovisor/bcc/blob/master/docs/tutorial.md)
+# [ChaikenSCALE2019.pdf](http://she-devel.com/ChaikenSCALE2019.pdf)
+# [Alison Chaiken's Embedded Linux Resources](http://she-devel.com/)
