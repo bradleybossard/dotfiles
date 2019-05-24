d33745d087488a86b2bbb2e6b144bcf84cca9de0
Bradley Bossard <bradleybossard@gmail.com>
2019-05-24 22:49:23 +0000

    Add notes for logging

--- install/install_ufw.sh
+++ install/install_ufw.sh
@@ -1,4 +1,7 @@
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-with-ufw-on-ubuntu-18-04
 sudo apt install --yes ufw
+
+
 sudo ufw default allow outgoing
 sudo ufw default deny incoming
 sudo ufw allow https
@@ -6,3 +9,6 @@ sudo ufw allow http
 sudo ufw allow ssh
 sudo ufw enable
 sudo ufw status
+
+# Enable logging
+# sudo ufw logging on
