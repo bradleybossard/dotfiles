3f23c94e79d18f9b94a7dd10b4f376d0b654ed59
Bradley Bossard <bradleybossard@gmail.com>
2019-05-24 22:43:31 +0000

    Removed redundant line

--- install/install_certbot.sh
+++ install/install_certbot.sh
@@ -1,6 +1,5 @@
-sudo add-apt-repository universe
-sudo add-apt-repository ppa:certbot/certbot
-sudo apt-get update
+sudo add-apt-repository --yes universe
+sudo add-apt-repository --yes ppa:certbot/certbot
+sudo apt-get update --fix-missing
 sudo apt-get install --yes certbot python-certbot-nginx
-sudo certbot --nginx
 sudo certbot --authenticator standalone --installer nginx -d bradleybossard.com -d www.bradleybossard.com --pre-hook "service nginx stop" --post-hook "service nginx start"
