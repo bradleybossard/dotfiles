4b5f72ef82fb2be3397e406bc63583dff374678b
Bradley Bossard <bradleybossard@gmail.com>
2019-05-27 19:18:58 -0700

    Add install for etcher

---
+++ install/install_etcher.sh
@@ -0,0 +1,4 @@
+echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+sudo apt-get update
+sudo apt-get install --yes balena-etcher-electron
