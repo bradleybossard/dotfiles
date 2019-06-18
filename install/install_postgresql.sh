846bdb1cb7f119a4c38bddb1a844fee4a8a278e1
Bradley Bossard <bradleybossard@gmail.com>
2019-06-18 20:46:38 +0000

    Add instructions for creating user/database

--- install/install_postgresql.sh
+++ install/install_postgresql.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 if [[ $OSTYPE == *"linux"* ]]; then
   sudo apt-get install --yes postgresql postgresql-client postgresql-client-common
-  # TODO: According to this tutorial, all that is needed is the following, need to verify
-  # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-18-04
-  # sudo apt install postgresql postgresql-contrib
+
+  echo "Upon successful installation, create a user and database with the following commands:"
+  echo "sudo -u postgres createuser <user>"
+  echo "sudo -u postgres createdb -O <user> dbname"
 fi
