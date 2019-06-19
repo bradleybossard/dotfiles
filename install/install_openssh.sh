7b606d6a2060e631b1843d56bd8ad1aee994e5f1
Bradley Bossard <bradleybossard@gmail.com>
2019-06-18 20:05:35 -0700

    Add tip for setting up 2FA

--- install/install_openssh.sh
+++ install/install_openssh.sh
@@ -1,3 +1,6 @@
+# Set up 2FA for SSH
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-multi-factor-authentication-for-ssh-on-ubuntu-16-04
+
 sudo apt-get install --yes --fix-broken
 
 sudo apt-get install --yes openssh-server
