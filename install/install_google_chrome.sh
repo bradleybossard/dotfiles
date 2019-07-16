c46216bf485cf290f7b25555f7dc9ef61559c1dd
Bradley Bossard <bradleybossard@gmail.com>
2019-07-16 13:19:23 -0700

    Move to /tmp before install

--- install/install_google_chrome.sh
+++ install/install_google_chrome.sh
@@ -1,3 +1,5 @@
 # Install Google Chrome on Ubuntu
+cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+cd -
