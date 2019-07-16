db4301c4413db6ba6946b40bc6c8a8504b9885be
Bradley Bossard <bradleybossard@gmail.com>
2019-07-16 13:24:21 -0700

    Move to /tmp for install

--- install/install_anaconda.sh
+++ install/install_anaconda.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+cd /tmp
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
 bash Anaconda3-5.3.1-Linux-x86_64.sh
 conda update conda
+cd -
+
+echo "Run > conda init"
