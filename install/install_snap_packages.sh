835abfb1608206615b62306aa7a5c2dff75ae66d
Bradley Bossard <bradleybossard@gmail.com>
2019-07-17 16:00:21 -0700

    Adding snap to list of snap packages to install

--- install/install_snap_packages.sh
+++ install/install_snap_packages.sh
@@ -1,9 +1,9 @@
 sudo snap install opentoonz
 sudo snap install obs-studio
 sudo snap install shotcut --classic
+sudo snap install hub --classic
 sudo snap install wormhole
 sudo snap install kata-containers --classic
 sudo snap install sickgear
 sudo snap install ultimate-media-downloader
-
 sudo snap install syncthing
