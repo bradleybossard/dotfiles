1bf55a96934fa92987e226c88b782f4a931b03ab
Bradley Bossard <bradleybossard@gmail.com>
2019-07-19 11:31:19 -0700

    Update docker install to use snap

--- install/install_docker.sh
+++ install/install_docker.sh
@@ -1,31 +1,11 @@
+# install docker via snap
+sudo snap install docker
 
+# create new system group, add $USER and login
+sudo addgroup --system docker
+sudo adduser $USER docker
+newgrp docker
 
-# Official Docker instructions
-# https://docs.docker.com/engine/installation/linux/ubuntu/#/prerequisites
-
-# omnibus installer
-#curl -sSL https://get.docker.com/ | sh
-
-sudo apt-get update --fix-missing
-
-sudo apt-get install --yes \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get update --fix-missing
-
-sudo apt-get install --yes docker-ce
-
-sudo docker run hello-world
-
-sudo usermod -aG docker $USER
-echo "Added user $USER group docker, must log out to take effect"
+# docker needs to be restarted to absorb group change
+sudo snap disable docker
+sudo snap enable docker
