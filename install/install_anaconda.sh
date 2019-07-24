932af7e886dc65be2e98f967606c182a5dc6c544
Bradley Bossard <bradleybossard@gmail.com>
2019-07-24 18:19:57 +0000

    Cleanup Anaconda install

--- install/install_anaconda.sh
+++ install/install_anaconda.sh
@@ -5,5 +5,12 @@ bash Anaconda3-5.3.1-Linux-x86_64.sh
 conda update conda
 conda update anaconda
 cd -
-
 echo "Run > conda init"
+exit
+
+## TODO: Experiment with Miniconda
+cd /tmp
+curl -O https://repo.anaconda.com/miniconda/Miniconda2-4.6.14-Linux-x86_64.sh
+bash Miniconda2-4.6.14-Linux-x86_64.sh
+# TODO: Add appropriate conda update equivalents as above
+cd -
