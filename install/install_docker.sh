1db496cabe2fab17697240fcf33410ce8eeef782
Bradley Bossard <bradleybossard@gmail.com>
2019-07-30 16:56:50 -0700

    Simply install

--- install/install_docker.sh
+++ install/install_docker.sh
@@ -1,11 +1,12 @@
-# install docker via snap
-sudo snap install docker
-
 # create new system group, add $USER and login
 sudo addgroup --system docker
 sudo adduser $USER docker
 newgrp docker
 
+# install docker via snap
+sudo snap install docker
+
+# TODO: No longer needed if user/group run before snap command.  To verify.
 # docker needs to be restarted to absorb group change
-sudo snap disable docker
-sudo snap enable docker
+#sudo snap disable docker
+#sudo snap enable docker
