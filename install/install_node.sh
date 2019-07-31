e73d42a97e7d844530884d31613ca7bf73b739ef
Bradley Bossard <bradleybossard@gmail.com>
2019-07-30 21:08:56 -0700

    Remove sudo from npm install

--- install/install_node.sh
+++ install/install_node.sh
@@ -2,4 +2,4 @@
 # set -euo pipefail
 
 sudo snap install node --channel=12/stable --classic
-sudo npm install -g git+https://github.com/ramitos/jsctags.git
+npm install -g git+https://github.com/ramitos/jsctags.git
