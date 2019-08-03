25a0bfd784581592ccd777d208406f4dcd383d81
Bradley Bossard <bradleybossard@gmail.com>
2019-08-03 21:13:08 +0000

    Verfiy and cleanup of docker install using snap

--- install/install_docker.sh
+++ install/install_docker.sh
@@ -1,12 +1,6 @@
-# create new system group, add $USER and login
 sudo addgroup --system docker
 sudo adduser $USER docker
-newgrp docker
-
+newgrp docker << END
 # install docker via snap
 sudo snap install docker
-
-# TODO: No longer needed if user/group run before snap command.  To verify.
-# docker needs to be restarted to absorb group change
-#sudo snap disable docker
-#sudo snap enable docker
+END
