0caeea5e28768a3faa43601a181f06959062f091
Bradley Bossard <bradleybossard@gmail.com>
2019-08-25 20:40:02 +0000

    Add install script for elixir

---
+++ install/install_elixir.sh
@@ -0,0 +1,7 @@
+PACKAGE_NAME=erlang-solutions_1.0_all.deb
+wget https://packages.erlang-solutions.com/$PACKAGE_NAME
+sudo dpkg -i $PACKAGE_NAME
+sudo apt-get update
+sudo apt-get install --yes esl-erlang
+sudo apt-get install --yes elixir
+rm $PACKAGE_NAME
