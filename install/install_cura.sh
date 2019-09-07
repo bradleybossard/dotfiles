7483d294ca821290483282a5794bd768d2557e8e
Bradley Bossard <bradleybossard@gmail.com>
2019-09-07 14:06:53 -0700

    Uncomment cura commands

--- install/install_cura.sh
+++ install/install_cura.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Ignore the PPA for now, best way seems to be downloading from site
 # In the future, check for a snap package
-# sudo add-apt-repository ppa:thopiekar/cura-master
-# sudo apt-get update
-# sudo apt-get install --yes cura
+sudo add-apt-repository ppa:thopiekar/cura-master
+sudo apt-get update
+sudo apt-get install --yes cura
