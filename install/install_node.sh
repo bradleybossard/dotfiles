a155d8a257214285836045e820648984810d0b21
Bradley Bossard <bradleybossard@gmail.com>
2019-10-21 22:26:39 +0000

    Updating scripts for global npm installs without sudo

--- install/install_node.sh
+++ install/install_node.sh
@@ -2,4 +2,6 @@
 # set -euo pipefail
 
 sudo snap install node --channel=12/stable --classic
+mkdir "${HOME}/.npm-packages"
+npm config set prefix "${HOME}/.npm-packages"
 npm install -g git+https://github.com/ramitos/jsctags.git
