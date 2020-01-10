ae57488fb34bfb7fc6b9f051ad43ab58bf61a590
Bradley Bossard <bradleybossard@gmail.com>
2020-01-09 20:51:46 -0800

    Updating scripts for install tags

--- install/install_go_tools.sh
+++ install/install_go_tools.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 go get github.com/Rican7/define            # CLI dictionary written in go
-go get github.com/aykamko/tag              # instantly jump to your ag matches
 go get github.com/github/hub               # hub - win at github
 go get -u github.com/tomnomnom/gron        # gron - flatten json
 go get -u github.com/maxzender/jv          # jv - interactive json viewer
