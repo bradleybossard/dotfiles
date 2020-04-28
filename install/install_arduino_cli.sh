f49da317d656c62b338cc2fda05e57d3f6fc73ce
Bradley Bossard <bradleybossard@gmail.com>
2020-04-28 17:01:47 +0000

    Remove unnecessary PATH parse

--- install/install_arduino_cli.sh
+++ install/install_arduino_cli.sh
@@ -1,6 +1,5 @@
   cd /tmp
   wget https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz
   tar -zxf arduino-cli_latest_Linux_64bit.tar.gz
-  tr ':' '\n' <<< "$PATH"
   sudo cp arduino-cli /usr/bin
   cd -
