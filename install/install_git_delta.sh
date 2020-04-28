9a5a5c4a093546ee3d85c5830e253c3411553803
Bradley Bossard <bradleybossard@gmail.com>
2020-04-28 17:02:04 +0000

    add git-delta install script

---
+++ install/install_git_delta.sh
@@ -0,0 +1,5 @@
+cd /tmp
+wget https://github.com/dandavison/delta/releases/download/0.1.1/delta-0.1.1-x86_64-unknown-linux-musl.tar.gz
+tar -zxf delta-0.1.1-x86_64-unknown-linux-musl.tar.gz
+sudo cp delta-0.1.1-x86_64-unknown-linux-musl/delta /usr/bin
+cd -
