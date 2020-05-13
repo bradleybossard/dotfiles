8cfecc71507dfc5ce38c8e31a48eea90faf321b7
Bradley Bossard <bradleybossard@gmail.com>
2020-05-13 10:35:51 -0700

    wipe out directory before install

--- install/install_pyenv.sh
+++ install/install_pyenv.sh
@@ -5,9 +5,13 @@ sudo apt-get install --yes make build-essential libssl-dev zlib1g-dev libbz2-dev
   libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
   xz-utils tk-dev libffi-dev liblzma-dev
 
+
+PYENV_ROOT=$HOME/.pyenv
+
+rm -rf $PYENV_ROOT
+
 # install pyenv
-# curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
 # install pyenv-virtualenv
-PYENV_ROOT=$HOME/.pyenv
 git clone https://github.com/pyenv/pyenv-virtualenv.git $PYENV_ROOT/plugins/pyenv-virtualenv
