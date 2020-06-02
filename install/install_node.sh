442ab5936774515f2929b2cb847bec68ab662c4b
Bradley Bossard <bradleybossard@gmail.com>
2020-06-02 11:32:10 -0700

    use nvm to install node

--- install/install_node.sh
+++ install/install_node.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # set -euo pipefail
 
-sudo snap install node --channel=12/stable --classic
-mkdir "${HOME}/.npm-packages"
-npm config set prefix "${HOME}/.npm-packages"
-npm install -g git+https://github.com/ramitos/jsctags.git
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+echo "open a new window and type"
+echo "nvm install --lts"
+
+#sudo snap install node --channel=12/stable --classic
+#mkdir "${HOME}/.npm-packages"
+#npm config set prefix "${HOME}/.npm-packages"
+#npm install -g git+https://github.com/ramitos/jsctags.git
