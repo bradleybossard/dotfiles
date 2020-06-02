05fd77a75aa551a96ea0250c3174745675292a23
Bradley Bossard <bradleybossard@gmail.com>
2020-06-02 11:31:51 -0700

    add install for archivebox

--- install/install_archivebox.sh
+++ install/install_archivebox.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+pushd .
 cd ~/src
 git clone https://github.com/pirate/ArchiveBox.git archivebox
 cd archivebox
 sudo apt install --yes python3 python3-pip git curl wget youtube-dl chromium-browser
+popd
