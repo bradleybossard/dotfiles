d6ecc7688606e52ab9a4cc4f0dbfca750f8c37e6
Bradley Bossard <bradleybossard@gmail.com>
2020-06-15 12:05:17 -0700

    Adding support for cheat

---
+++ install/install_cheat.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd ~/src/dotfiles
+pushd .
+cd /tmp
+
+VERSION=3.10.1
+
+if [[ $OSTYPE == *"darwin"* ]]; then
+  ARCH=darwin-amd64
+  USER_BIN_DIR=/usr/local/bin/
+fi
+
+if [[ $OSTYPE == *"linux"* ]]; then
+  ARCH=linux-amd64
+  USER_BIN_DIR=/usr/local/bin/
+fi
+
+wget -O  - https://github.com/cheat/cheat/releases/download/$VERSION/cheat-$ARCH.gz | gunzip -c > cheat
+chmod 755 cheat
+mv cheat $USER_BIN_DIR
+popd
+mkdir -p ~/.config/cheat
+mv cheat_conf.yml ~/.config/cheat/conf.yml
