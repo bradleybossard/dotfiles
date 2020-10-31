fa239fac61389a07e69c3e546e192a5832b8373a
Bradley Bossard <bradleybossard@gmail.com>
2020-10-30 17:57:19 -0700

    add vpn elemnets prior to PIA install

--- install/install_pia.sh
+++ install/install_pia.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 pushd .
 cd /tmp
+sudo apt-get install --yes openvpn network-manager-openvpn network-manager-openvpn-gnome
 wget https://www.privateinternetaccess.com/installer/pia-nm.sh
 sudo bash pia-nm.sh
 popd
