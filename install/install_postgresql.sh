fa06eda960c612ae89ea62e768485a57f93fdadd
Bradley Bossard <bradleybossard@gmail.com>
2020-11-15 20:45:49 -0800

    Call apt update before install

--- install/install_postgresql.sh
+++ install/install_postgresql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ $OSTYPE == *"linux"* ]]; then
-  sudo apt-get install --yes postgresql postgresql-client postgresql-client-common
+  sudo apt update --fix-missing
+  sudo apt install --yes postgresql postgresql-client postgresql-client-common
 
   echo "Upon successful installation, create a user and database with the following commands:"
   echo "sudo -u postgres createuser <user>"
