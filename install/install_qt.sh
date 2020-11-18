99c7a68d775c8d02789efebcc47ec4f112e8e481
Bradley Bossard <bradleybossard@gmail.com>
2020-11-18 15:12:33 -0800

    add qt install script

---
+++ install/install_qt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install --yes build-essential qtcreator qt5-default
+exit
+
+# TODO: haven't testing this
+sudo apt-get install --yes qt5-doc qt5-doc-html qtbase5-doc-html qtbase5-examples
