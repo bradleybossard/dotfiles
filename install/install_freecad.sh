bf6a4413ef1a0dc123717cfe3cdabbe399b65c1e
Bradley Bossard <bradleybossard@gmail.com>
2020-12-23 09:29:45 -0800

    some new installs

--- install/install_freecad.sh
+++ install/install_freecad.sh
@@ -1,3 +1,3 @@
-sudo add-apt-repository ppa:freecad-maintainers/freecad-stable
+sudo add-apt-repository --yes ppa:freecad-maintainers/freecad-stable
 sudo apt-get update --fix-missing
-sudo apt install --yes freecad freecad-common
+sudo apt install --yes --fix-missing freecad freecad-common
