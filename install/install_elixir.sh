82f0ffa02d28f36b76ddc14369adfce561905d24
Bradley Bossard <bradleybossard@gmail.com>
2021-02-13 19:46:18 -0800

    cleanup downloaded file

--- install/install_elixir.sh
+++ install/install_elixir.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+pushd .
+cd /tmp
 PACKAGE_NAME=erlang-solutions_2.0_all.deb
 wget https://packages.erlang-solutions.com/$PACKAGE_NAME
 sudo dpkg -i $PACKAGE_NAME
 sudo apt-get update
 sudo apt-get install --yes esl-erlang
 sudo apt-get install --yes elixir
-rm $PACKAGE_NAME
+popd
