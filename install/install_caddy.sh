408c834bdb8d8703c8ef71137765a84b2d955866
Bradley Bossard <bradleybossard@gmail.com>
2021-08-20 10:27:45 -0700

    add caddy install script

---
+++ install/install_caddy.sh
@@ -0,0 +1,5 @@
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/cfg/gpg/gpg.155B6D79CA56EA34.key' | sudo apt-key add -
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/cfg/setup/config.deb.txt?distro=debian&version=any-version' | sudo tee -a /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
