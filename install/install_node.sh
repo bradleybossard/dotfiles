3ad4e774674f535bfe26747a1a8c40357d912c4a
Bradley Bossard <bradleybossard@gmail.com>
2021-10-21 10:50:19 -0700

    update nvm install version

--- install/install_node.sh
+++ install/install_node.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-# set -euo pipefail
+set -euo pipefail
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
 echo "open a new window and type"
 echo "nvm install --lts"
-
-#sudo snap install node --channel=12/stable --classic
-#mkdir "${HOME}/.npm-packages"
-#npm config set prefix "${HOME}/.npm-packages"
-#npm install -g git+https://github.com/ramitos/jsctags.git
