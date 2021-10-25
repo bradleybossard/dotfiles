e05c9f290ec99fb5a9802c60b4aded7053523a61
Bradley Bossard <bradleybossard@gmail.com>
2021-10-25 10:52:29 -0700

    add script for tweeking gnome

---
+++ install/install_gnome_extensions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Installs the Gnome Extensions app
+sudo apt install --yes gnome-shell-extensions
+# Allows installation of extensions via https://extensions.gnome.org/
+sudo apt-get install --yes chrome-gnome-shell
+
+# https://extensions.gnome.org/extension/307/dash-to-dock/
