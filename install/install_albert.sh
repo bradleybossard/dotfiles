6cfc963087c9ce5b8772ac2d41b28a31e8d091c4
Bradley Bossard <bradleybossard@gmail.com>
2021-12-13 09:23:43 -0800

    update albert install script

--- install/install_albert.sh
+++ install/install_albert.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install albert
+sudo apt-get install --yes albert
