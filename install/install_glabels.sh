8826d911533248e0c6b1e392b177804edf52bb6d
Bradley Bossard <bradleybossard@gmail.com>
2021-12-27 11:27:19 -0800

    do not prompt for install

--- install/install_glabels.sh
+++ install/install_glabels.sh
@@ -1,4 +1,4 @@
-sudo add-apt-repository ppa:krisives/glabels-qt
-sudo apt-get update
+sudo add-apt-repository --yes ppa:krisives/glabels-qt
+sudo apt-get --yes update
 
 sudo apt install --yes glabels-qt
