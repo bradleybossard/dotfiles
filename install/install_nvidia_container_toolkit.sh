b1a39e19474302a6d277a0650bdfa8544b8832b7
Bradley Bossard <bradleybossard@gmail.com>
2021-12-27 11:28:10 -0800

    update instructions to get distro id correct

--- install/install_nvidia_container_toolkit.sh
+++ install/install_nvidia_container_toolkit.sh
@@ -8,8 +8,7 @@ cd /tmp
 #  && sudo systemctl start docker \
 #  && sudo systemctl enable docker
 
-#distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-distribution=$(. /etc/os-release;echo $ID_LIKE$VERSION_ID) \
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
    && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
