d3a74905dc7371e54c9f11de597b89f0f4f246f4
Bradley Bossard <bradleybossard@gmail.com>
2022-01-19 06:48:22 -0800

    add user to audio group for realtime use

--- install/install_guitarix.sh
+++ install/install_guitarix.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 sudo apt install --yes guitarix
+
+# Needed for realtime tuner use in guitarix
+sudo adduser $USER audio
+
+# might require restart
--- install/install_jackd.sh
+++ install/install_jackd.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 sudo apt install --yes jackd
+
+# Needed for realtime tuner use in guitarix
+sudo adduser $USER audio
+
+# might require restart
