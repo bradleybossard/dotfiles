a0b50e5e398ef69d40529b1854ef12ecc031cd3f
Bradley Bossard <bradleybossard@gmail.com>
2022-05-29 10:34:16 -0700

    auto install brave install dependencies

--- install/install_brave_browser.sh
+++ install/install_brave_browser.sh
@@ -1,4 +1,4 @@
-sudo apt install apt-transport-https curl
+sudo apt install --yes apt-transport-https curl
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
