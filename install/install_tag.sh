0e8505b2696c5320b0ec985f9e0b8ba8729a8479
Bradley Bossard <bradleybossard@gmail.com>
2022-06-10 16:05:01 -0700

    fix tag install

--- install/install_tag.sh
+++ install/install_tag.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# TODO: try linux command on osx, simplify
-exit
 
 # tag - instantly jump to your ag/rg matches
 if [[ $OSTYPE == *"linux"* ]]; then
   go install github.com/aykamko/tag@latest
 fi
+
+# TODO: try linux command on osx, simplify
+exit
 
 if [[ $OSTYPE == *"darwin"* ]]; then
   brew tap aykamko/tag-ag
