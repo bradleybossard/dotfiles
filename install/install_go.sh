267ab099e16e0fd0cf976bf55bb0f6aa362baeb9
Bradley Bossard <bradleybossard@gmail.com>
2022-06-10 16:04:23 -0700

    update golang install instructions

--- install/install_go.sh
+++ install/install_go.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 if [[ $OSTYPE == *"linux"* ]]; then
-  sudo snap install --classic go
+  if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+  fi
+
+  # TODO: Ensure sudo before script runs
+  cd /tmp
+  FILE=go1.18.3.linux-amd64.tar.gz
+  wget "https://go.dev/dl/$FILE"
+  rm -rf /usr/local/go && tar -C /usr/local -xzf "$FILE"
+  cd -
 fi
 
 if [[ $OSTYPE == *"darwin"* ]]; then
+  # TODO: Probably good enough, check version when finished.
+  # If not, implement instructions as above for official install.
   brew install go
 fi
