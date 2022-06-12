0180f2333f2e8b58ad3aa3dafef133a67de0d607
Bradley Bossard <bradleybossard@gmail.com>
2022-06-12 09:46:04 -0700

    add command for updating registry config

--- install/install_podman.sh
+++ install/install_podman.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-sudo apt install --yes podman
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+  echo "Not running as root"
+  exit
+fi
+
+apt install --yes podman
+
+# This is needed b/c podman isn't setup to talk to dockerhub by default
+# TODO: ideally this is a sed command to replace the existing commented out line
+echo 'unqualified-search-registries=["docker.io"]' >> /etc/containers/registries.conf
+
