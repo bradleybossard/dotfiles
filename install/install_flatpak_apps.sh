cf9d1142d27ce816390e89746fc63d258444aa74
Bradley Bossard <bradleybossard@gmail.com>
2022-06-12 09:38:24 -0700

    list of apps to install

--- install/install_flatpak_apps.sh
+++ install/install_flatpak_apps.sh
@@ -1,3 +1,15 @@
-sudo flatpak install --system --assumeyes flathub org.inkscape.Inkscape
-sudo flatpak install --system --assumeyes flathub com.visualstudio.code
-sudo flatpak install --system --assumeyes flathub org.zealdocs.Zeal
+# sudo flatpak install --system --assumeyes flathub com.visualstudio.code
+# sudo flatpak install --system --assumeyes flathub org.zealdocs.Zeal
+
+flatpak install --noninteractive flathub com.usebottles.bottles
+flatpak install --noninteractive flathub org.inkscape.Inkscape
+flatpak install --noninteractive flathub com.agateau.PixelWheels
+flatpak install --noninteractive flathub io.github.maurycyliebner.enve
+flatpak install --noninteractive flathub org.tuxfamily.StuntRally
+flatpak install --noninteractive flathub com.github.KRTirtho.Spotube
+flatpak install --noninteractive flathub app.xemu.xemu
+flatpak install --noninteractive flathub org.synfig.SynfigStudio
+flatpak install --noninteractive flathub org.freecadweb.FreeCAD
+flatpak install --noninteractive flathub io.github.OpenToonz
+flatpak install --noninteractive flathub org.freecadweb.FreeCAD
+flatpak install --noninteractive flathub org.kicad.KiCad
