c0f19c3ec168280104c115376e10a32921c341ed
Bradley Bossard <bradleybossard@gmail.com>
2022-07-02 07:52:53 -0700

    add list of flatpak apps to install

--- install/install_flatpak_apps.sh
+++ install/install_flatpak_apps.sh
@@ -13,3 +13,19 @@ flatpak install --noninteractive flathub org.freecadweb.FreeCAD
 flatpak install --noninteractive flathub io.github.OpenToonz
 flatpak install --noninteractive flathub org.freecadweb.FreeCAD
 flatpak install --noninteractive flathub org.kicad.KiCad
+
+flatpak install --noninteractive flathub net.sourceforge.dmidiplayer
+flatpak install --noninteractive flathub com.github.wwmm.easyeffects
+flatpak install --noninteractive flathub net.sourceforge.kmidimon
+flatpak install --noninteractive flathub net.sourceforge.kmetronome
+flatpak install --noninteractive flathub com.github.geigi.cozy
+flatpak install --noninteractive flathub com.github.artemanufrij.playmyvideos
+flatpak install --noninteractive flathub io.github.cmus.cmus
+flatpak install --noninteractive flathub com.github.alainm23.byte
+flatpak install --noninteractive flathub sa.sy.bluerecorder
+flatpak install --noninteractive flathub com.bitwig.BitwigStudio
+flatpak install --noninteractive flathub org.audacityteam.Audacity
+flatpak install --noninteractive flathub ca.littlesvr.asunder
+flatpak install --noninteractive flathub com.rafaelmardojai.Blanket
+flatpak install --noninteractive flathub org.gnome.Cheese
+
