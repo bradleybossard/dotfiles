7a15e032a371e3bf36becb9e9497f402c15f5ff3
Bradley Bossard <bradleybossard@gmail.com>
2022-07-02 07:51:48 -0700

    add command to install image app launcher

--- install/install_app_image_launcher.sh
+++ install/install_app_image_launcher.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
 gh install TheAssassin/AppImageLauncher
+
+# Run command to install after downloaded via gh install
+~/.local/bin/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage install
+
