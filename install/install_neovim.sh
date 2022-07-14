ea556f5b61e706e1cc3daccafa008263c1eebea4
Bradley Bossard <bradleybossard@gmail.com>
2022-07-14 12:09:32 -0700

    cleanup cruft in neovim script

--- install/install_neovim.sh
+++ install/install_neovim.sh
@@ -9,10 +9,3 @@ sudo apt-get install --yes neovim
 
 # Setup neovim as permanent alternative to vim
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-#sudo update-alternatives --config vi
-exit
-
-# These may yet be needed
-# sudo apt-get install --yes software-properties-common
-# sudo apt-get install --yes python-software-properties
-# sudo apt-get install --yes python-dev python-pip python3-dev python3-pip
