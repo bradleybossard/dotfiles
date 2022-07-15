52b0e3f17f68ebb5261192cc961485fbea50517d
Bradley Bossard <bradleybossard@gmail.com>
2022-07-14 21:34:21 -0700

    updating docker-compose install instructions for podman

--- install/install_docker_compose.sh
+++ install/install_docker_compose.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+pushd .
+
+cd /tmp
+
 # GET the latest release using the Github API
 VERSION=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq '.name' | sed 's/"//g')
 echo $VERSION
@@ -5,3 +11,10 @@ echo $VERSION
 sudo curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+# this is needed when using podman instead of docker
+systemctl --user start podman.socket
+
+popd
+
+exit
