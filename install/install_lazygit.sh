70340d68f7fe8342dd0254aad9ceffc13f6b0352
Bradley Bossard <bradleybossard@gmail.com>
2022-07-20 11:14:06 -0700

    use gh install for lazygit

--- install/install_lazygit.sh
+++ install/install_lazygit.sh
@@ -1,3 +1,6 @@
-sudo add-apt-repository --yes ppa:lazygit-team/release
-sudo apt-get update
-sudo apt-get install --yes lazygit
+!/bin/bash
+gh install jesseduffield/lazygit
+
+# sudo add-apt-repository --yes ppa:lazygit-team/release
+# sudo apt-get update
+# sudo apt-get install --yes lazygit
