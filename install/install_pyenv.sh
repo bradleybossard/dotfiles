bfbfc835b5de6dce2a0c7b526b187d9e7c6b1bba
Bradley Bossard <bradleybossard@gmail.com>
2022-08-15 09:12:59 -0700

    update pyenv install script

--- install/install_pyenv.sh
+++ install/install_pyenv.sh
@@ -5,13 +5,9 @@ sudo apt-get install --yes make build-essential libssl-dev zlib1g-dev libbz2-dev
   libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
   xz-utils tk-dev libffi-dev liblzma-dev
 
-
 PYENV_ROOT=$HOME/.pyenv
 
 rm -rf $PYENV_ROOT
 
 # install pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-
-# install pyenv-virtualenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git $PYENV_ROOT/plugins/pyenv-virtualenv
