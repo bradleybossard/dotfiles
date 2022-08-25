f31eceaaf5bb521f71cedc22b1f1a0f8de9886c0
Bradley Bossard <bradleybossard@gmail.com>
2022-08-25 10:45:02 -0700

    improve texlive install process

--- install/install_texlive.sh
+++ install/install_texlive.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-cd tmp
+pushd .
+cd /tmp
 wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -zxf install-tl-unx.tar.gz
-cd install-tl-unx
+UNTARRED_DIR=$(find . -type d -name "install-tl-[0-9]*" | head -1)
+cd $UNTARRED_DIR
 sudo ./install-tl
 # TODO: More research to initial non-interactive install
 # https://www.tug.org/texlive/doc/install-tl.html
-cd -
+popd
 
-echo "After installing, to use TexStudio, the following must be configured"
-echo "Go to Options -> Configure -> Build, then click "Advanced Options and "
-echo "put the texLive install path, i.e. "
-echo ""
-echo "/usr/local/texlive/2022/bin/x86_64-linux"
-echo ""
-echo "in the Commands field"
+echo 'After installing, to use TexStudio, the following must be configured'
+echo 'Go to Options -> Configure -> Build, then click "Advanced Options and '
+echo 'put the texLive install path, i.e. '
+echo ''
+echo 'usr/local/texlive/2022/bin/x86_64-linux'
+echo ''
+echo 'in the Commands field'
 
 
