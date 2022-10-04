ba821096f06933a424e1356bccc9af34612a9cc6
Bradley Bossard <bradleybossard@gmail>
2022-10-04 13:10:47 -0700

    add install scripts

---
+++ install/install_kazam.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo add-apt-repository --yes ppa:sylvain-pineau/kazam
+sudo apt-get update
+sudo apt install --yes kazam
---
+++ install/install_obs_studio.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo add-apt-repository --yes ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install --yes obs-studio
