3869a0c49c3e1935ab54402516ce27ccd28564bc
Bradley Bossard <bradleybossard@gmail.com>
2022-11-06 10:44:52 -0800

    adding more flatpak

--- install/install_flatpak_apps.sh
+++ install/install_flatpak_apps.sh
@@ -1,18 +1,22 @@
 # sudo flatpak install --system --assumeyes flathub com.visualstudio.code
 # sudo flatpak install --system --assumeyes flathub org.zealdocs.Zeal
 
-flatpak install --noninteractive flathub com.usebottles.bottles
+
+flatpak install --noninteractive flathub com.visualstudio.code
+flatpak install --noninteractive flathub org.freecadweb.FreeCAD
 flatpak install --noninteractive flathub org.inkscape.Inkscape
+flatpak install --noninteractive flathub org.kicad.KiCad
+flatpak install --noninteractive flathub io.github.OpenToonz
+flatpak install --noninteractive flathub org.texstudio.TeXstudio
+exit
+
+flatpak install --noninteractive flathub com.usebottles.bottles
 flatpak install --noninteractive flathub com.agateau.PixelWheels
 flatpak install --noninteractive flathub io.github.maurycyliebner.enve
 flatpak install --noninteractive flathub org.tuxfamily.StuntRally
 flatpak install --noninteractive flathub com.github.KRTirtho.Spotube
 flatpak install --noninteractive flathub app.xemu.xemu
 flatpak install --noninteractive flathub org.synfig.SynfigStudio
-flatpak install --noninteractive flathub org.freecadweb.FreeCAD
-flatpak install --noninteractive flathub io.github.OpenToonz
-flatpak install --noninteractive flathub org.freecadweb.FreeCAD
-flatpak install --noninteractive flathub org.kicad.KiCad
 
 flatpak install --noninteractive flathub net.sourceforge.dmidiplayer
 flatpak install --noninteractive flathub com.github.wwmm.easyeffects
