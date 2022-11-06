dced93fe2778d4a2032089c591c56b3823b5056d
Bradley Bossard <bradleybossard@gmail.com>
2022-11-06 10:45:11 -0800

    adding --yes to apt commands

--- install/install_ros2_humble_hawksbill.sh
+++ install/install_ros2_humble_hawksbill.sh
@@ -3,14 +3,14 @@
 sudo apt install --yes software-properties-common
 sudo add-apt-repository universe --yes
 
-sudo apt update && sudo apt install curl gnupg lsb-release
+sudo apt update && sudo apt install --yes curl gnupg lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 sudo apt update
 
-sudo apt upgrade
+sudo apt upgrade --yes
 
 sudo apt install --yes ros-humble-desktop
 
