8eece927231b7ff2614c683a6932633aba0934f7
Bradley Bossard <bradleybossard@gmail.com>
2022-11-09 08:42:23 -0800

    remove steam install script

--- install/install_steam.sh
+++
@@ -1,7 +0,0 @@
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt-get update
-# TODO: Figure out how to query apt-get for latest driver version
-sudo apt install --yes nvidia-driver-396
-sudo reboot
-
-sudo apt install --yes steam
