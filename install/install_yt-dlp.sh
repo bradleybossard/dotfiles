78dbeeada59a069026b4c6a0fe3888ceecea92b3
Bradley Bossard <bradleybossard@gmail.com>
2022-11-22 21:30:42 -0800

    fixed typo

--- install/install_yt-dlp.sh
+++ install/install_yt-dlp.sh
@@ -1,3 +1,3 @@
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-sudo chmod a+rx /usr/local/bin/yt-dlp:w
+sudo chmod a+rx /usr/local/bin/yt-dlp
 
