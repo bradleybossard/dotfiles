7b87f17cb9b135a8e6aa9d2589aabc1b3c69f054
Bradley Bossard <bradleybossard@gmail.com>
2022-12-10 07:23:07 -0800

    update PIA install

--- install/install_pia.sh
+++ install/install_pia.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 pushd .
 cd /tmp
-sudo apt-get install --yes openvpn network-manager-openvpn network-manager-openvpn-gnome
-wget https://www.privateinternetaccess.com/installer/pia-nm.sh
-sudo bash pia-nm.sh
+# Won't get the latest version, but should prompt to upgrade
+wget https://installers.privateinternetaccess.com/download/pia-linux-3.3.1-06924.run
+chmod 755 pia-linux*
+./pia-linux*
 popd
