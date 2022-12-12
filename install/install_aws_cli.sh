f719952dcaff738650b4901728ec14256d200a84
Bradley Bossard <bradleybossard@gmail.com>
2022-12-12 14:34:06 -0800

    install aws completions

--- install/install_aws_cli.sh
+++ install/install_aws_cli.sh
@@ -4,4 +4,8 @@ cd /tmp
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+# install completions
+complete -C '/usr/local/bin/aws_completer' aws
+
 popd
