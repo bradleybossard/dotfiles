75702c69620cb9d38e1cb3fec9f850d5d06e36d9
Bradley Bossard <bradleybossard@gmail.com>
2022-12-15 08:52:21 -0800

    add command to create venvs in project folder

--- install/install_poetry.sh
+++ install/install_poetry.sh
@@ -5,3 +5,7 @@ curl -sSL https://install.python-poetry.org | python3 -
 mkdir -p ~/.zfunc
 poetry completions zsh > ~/.zfunc/_poetry
 
+
+# This makes poetry create venvs in the project dir
+# Otherwise VS Code doesn't find them under ~/.cache/pypoetry
+poetry config virtualenvs.in-project true
