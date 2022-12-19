96c4e47409e9eef9e0ed44ba511ebef356b90f44
Bradley Bossard <bradleybossard@gmail.com>
2022-12-19 11:57:59 -0800

    update elixir install via asdf

--- install/install_elixir.sh
+++ install/install_elixir.sh
@@ -1,4 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+asdf plugin add elixir
+asdf plugin add erlang
+
+asdf list all elixir | grep main-otp
+# choose from latest main-otp- &&
+# asdf install elixir main-otp-XX
+
+# then install corresponding erlang
+# asdf list all erlang
+# asdf install erlang XX.X
+
+exit
+
 pushd .
 cd /tmp
 PACKAGE_NAME=erlang-solutions_2.0_all.deb
