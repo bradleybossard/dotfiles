0041cdf0ba683469f3909ff63054cbd640b3fec5
Bradley Bossard <bradleybossard@gmail.com>
2023-01-08 15:14:40 -0800

    add fuse install directions

--- install/install_app_image_launcher.sh
+++ install/install_app_image_launcher.sh
@@ -5,3 +5,6 @@ gh install TheAssassin/AppImageLauncher
 # Run command to install after downloaded via gh install
 ~/.local/bin/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage install
 
+# AppImages require FUSE to run
+sudo add-apt-repository universe
+sudo apt install --yes libfuse2
