034999160aae40e910c5878a670381a67c8a48dd
Bradley Bossard <bradleybossard@gmail.com>
2023-01-20 07:13:24 -0800

    add poetry config to play nice with asdf

--- install/install_poetry.sh
+++ install/install_poetry.sh
@@ -9,3 +9,4 @@ poetry completions zsh > ~/.zfunc/_poetry
 # This makes poetry create venvs in the project dir
 # Otherwise VS Code doesn't find them under ~/.cache/pypoetry
 poetry config virtualenvs.in-project true
+poetry config virtualenvs.prefer-active-python true
