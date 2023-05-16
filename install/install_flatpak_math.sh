1bda724239fd771423f53c35543676ca68441d2a
Bradley Bossard <bradleybossard@gmail.com>
2023-05-16 16:36:21 -0700

    organizing flatpak

---
+++ install/install_flatpak_math.sh
@@ -0,0 +1,10 @@
+flatpak install flathub org.octave.Octave
+
+flatpak install flathub org.jupyter.JupyterLab
+
+flatpak install --noninteractive flathub org.geogebra.GeoGebra
+
+# fractals
+flatpak install --noninteractive flathub io.github.xaos_project.XaoS
+flatpak install --noninteractive flathub com.github.buddhi1980.mandelbulber2
+
