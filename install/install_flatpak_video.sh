edfc48671d1520d5f7c47ede2c75beec3b7afe1e
Bradley Bossard <bradleybossard@gmail.com>
2023-05-18 06:50:14 -0700

    updating video apps

--- install/install_flatpak_video.sh
+++ install/install_flatpak_video.sh
@@ -8,3 +8,9 @@ flatpak install --noninteractive flathub org.openshot.OpenShot
 # motion graphics
 flatpak install --noninteractive flathub fr.natron.Natron
 
+# media server
+flatpak install --noninteractive flathub org.jellyfin.JellyfinServer
+
+# media clients
+flatpak install --noninteractive flathub tv.plex.PlexDesktop
+flatpak install --noninteractive flathub com.github.iwalton3.jellyfin-media-player
