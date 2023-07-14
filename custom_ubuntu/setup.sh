9d006fcd4decd35ab96040e77a6924c752226d9f
Bradley Bossard <bradleybossard@gmail.com>
2023-07-14 11:27:58 -0700

    add script for building custom ubuntu

---
+++ custom_ubuntu/setup.sh
@@ -0,0 +1,187 @@
+add-apt-repository --yes universe
+apt update --fix-missing
+
+apt-get install --yes \
+autojump \
+apt-file \
+curl \
+exuberant-ctags \
+git \
+jq \
+ncdu \
+neovim \
+rename \
+ripgrep \
+tree \
+tmux \
+tig \
+wget \
+xclip \
+zsh
+
+apt-file update
+
+add-apt-repository --yes ppa:flatpak/stable
+apt update --fix-missing
+apt install --yes flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# 2d
+flatpak install --noninteractive flathub org.kde.krita
+flatpak install --noninteractive flathub com.xnview.XnRetro
+flatpak install --noninteractive flathub org.gimp.GIMP
+flatpak install --noninteractive flathub io.github.maurycyliebner.enve
+flatpak install --noninteractive flathub org.synfig.SynfigStudio
+flatpak install --noninteractive flathub io.github.OpenToonz
+flatpak install --noninteractive flathub org.inkscape.Inkscape
+flatpak install --noninteractive flathub com.boxy_svg.BoxySVG
+flatpak install --noninteractive flathub com.rawtherapee.RawTherapee
+
+
+add-apt-repository --yes universe
+apt update --fix-missing
+
+apt-get install --yes \
+autojump \
+apt-file \
+curl \
+exuberant-ctags \
+git \
+jq \
+ncdu \
+neovim \
+rename \
+ripgrep \
+tree \
+tmux \
+tig \
+wget \
+xclip \
+zsh
+
+apt-file update
+
+add-apt-repository --yes ppa:flatpak/stable
+apt update --fix-missing
+apt install --yes flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# 2d
+flatpak install --noninteractive flathub org.kde.krita
+flatpak install --noninteractive flathub com.xnview.XnRetro
+flatpak install --noninteractive flathub org.gimp.GIMP
+flatpak install --noninteractive flathub io.github.maurycyliebner.enve
+flatpak install --noninteractive flathub org.synfig.SynfigStudio
+flatpak install --noninteractive flathub io.github.OpenToonz
+flatpak install --noninteractive flathub org.inkscape.Inkscape
+flatpak install --noninteractive flathub com.boxy_svg.BoxySVG
+flatpak install --noninteractive flathub com.rawtherapee.RawTherapee
+
+add-apt-repository --yes universe
+apt update --fix-missing
+
+apt-get install --yes \
+autojump \
+apt-file \
+curl \
+exuberant-ctags \
+git \
+jq \
+ncdu \
+neovim \
+rename \
+ripgrep \
+tree \
+tmux \
+tig \
+wget \
+xclip \
+zsh
+
+apt-file update
+
+add-apt-repository --yes ppa:flatpak/stable
+apt update --fix-missing
+apt install --yes flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# 2d
+flatpak install --noninteractive flathub org.kde.krita
+flatpak install --noninteractive flathub com.xnview.XnRetro
+flatpak install --noninteractive flathub org.gimp.GIMP
+flatpak install --noninteractive flathub io.github.maurycyliebner.enve
+flatpak install --noninteractive flathub org.synfig.SynfigStudio
+flatpak install --noninteractive flathub io.github.OpenToonz
+flatpak install --noninteractive flathub org.inkscape.Inkscape
+flatpak install --noninteractive flathub com.boxy_svg.BoxySVG
+flatpak install --noninteractive flathub com.rawtherapee.RawTherapee
+
+# 3d
+flatpak install --noninteractive flathub org.blender.Blender
+flatpak install --noninteractive flathub org.freecadweb.FreeCAD
+flatpak install --noninteractive flathub io.github.RodZill4.Material-Maker
+flatpak install --noninteractive flathub org.librecad.librecad
+flatpak install --noninteractive flathub com.wings3d.WINGS
+flatpak install --noninteractive flathub com.ultimaker.cura
+
+# audio
+flatpak install --noninteractive flathub net.sourceforge.dmidiplayer
+flatpak install --noninteractive flathub org.pipewire.Helvum
+flatpak install --noninteractive flathub com.github.wwmm.easyeffects
+flatpak install --noninteractive flathub net.sourceforge.kmetronome
+flatpak install --noninteractive flathub net.sourceforge.kmidimon
+flatpak install --noninteractive flathub com.github.geigi.cozy
+flatpak install --noninteractive flathub com.github.KRTirtho.Spotube
+flatpak install --noninteractive flathub io.github.cmus.cmus
+flatpak install --noninteractive flathub com.github.alainm23.byte
+flatpak install --noninteractive flathub de.haeckerfelix.Shortwave
+flatpak install --noninteractive flathub io.gitlab.zehkira.Monophony
+flatpak install --noninteractive flathub org.hydrogenmusic.Hydrogen
+flatpak install --noninteractive flathub org.audacityteam.Audacity
+flatpak install --noninteractive flathub org.tenacityaudio.Tenacity
+flatpak install --noninteractive flathub org.ardour.Ardour
+flatpak install --noninteractive flathub org.kde.kdenlive
+flatpak install --noninteractive flathub com.rosegardenmusic.rosegarden
+flatpak install --noninteractive flathub ca.littlesvr.asunder
+flatpak install --noninteractive flathub org.musescore.MuseScore
+flatpak install --noninteractive flathub io.github.muse_sequencer.Muse
+flatpak install --noninteractive flathub com.rafaelmardojai.Blanket
+flatpak install --noninteractive flathub org.mixxx.Mixxx
+flatpak install --noninteractive flathub io.github.dagargo.Elektroid
+flatpak install --noninteractive flathub org.nicotine_plus.Nicotine
+flatpak install --noninteractive flathub info.puredata.Pd-extended
+flatpak install --noninteractive flathub net.purrdata.PurrData
+
+# books
+flatpak install --noninteractive flathub com.calibre_ebook.calibre
+flatpak install --noninteractive flathub com.github.johnfactotum.Foliate
+flatpak install --noninteractive flathub com.github.babluboy.bookworm
+flatpak install --noninteractive flathub com.github.geigi.cozy
+
+# developer
+flatpak install --noninteractive flathub org.wezfurlong.wezterm
+flatpak install --noninteractive flathub com.github.mdh34.quickdocs
+flatpak install --noninteractive flathub com.getpostman.Postman
+flatpak install --noninteractive flathub dev.lapce.lapce
+flatpak install --noninteractive flathub com.redis.RedisInsight
+flatpak install --noninteractive flathub org.sqlitebrowser.sqlitebrowser
+flatpak install --noninteractive flathub io.dbeaver.DBeaverCommunity
+flatpak install --noninteractive flathub io.beekeeperstudio.Studio
+
+# electronics
+flatpak install --noninteractive flathub cc.arduino.IDE2
+flatpak install --noninteractive flathub org.fritzing.Fritzing
+flatpak install --noninteractive flathub org.kicad.KiCad
+
+# emulators
+flatpak install --noninteractive flathub org.ryujinx.Ryujinx
+flatpak install --noninteractive flathub org.yuzu_emu.yuzu
+
+# gamedev
+flatpak install --noninteractive flathub org.godotengine.Godot
+flatpak install --noninteractive flathub com.orama_interactive.Pixelorama
+flatpak install --noninteractive flathub com.unity.UnityHub
+flatpak install --noninteractive flathub io.github.achetagames.epic_asset_manager
+flatpak install --noninteractive flathub org.twinery.Twine
+flatpak install --noninteractive flathub com.inform7.IDE
+
