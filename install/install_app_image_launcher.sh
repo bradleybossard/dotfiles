dd686ad6fee2d28f1d044a33e467b5042cfac52a
Bradley Bossard <bradleybossard@gmail.com>
2023-07-15 08:14:36 -0700

    update app image launcher script

--- install/install_app_image_launcher.sh
+++ install/install_app_image_launcher.sh
@@ -1,10 +1,8 @@
-#/bin/bash
+#!/usr/bin/env bash
 
+# choose .deb package
 gh install TheAssassin/AppImageLauncher
 
-# Run command to install after downloaded via gh install
-~/.local/bin/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage install
-
 # AppImages require FUSE to run
-sudo add-apt-repository universe
+sudo add-apt-repository --yes universe
 sudo apt install --yes libfuse2
