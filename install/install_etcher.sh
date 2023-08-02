8b79942e9c7e8694f2e3458255f36a361d2ec39f
Bradley Bossard <bradleybossard@gmail.com>
2023-08-02 11:19:40 -0700

    update etcher install method

--- install/install_etcher.sh
+++ install/install_etcher.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-sudo apt-get update
-sudo apt-get install --yes balena-etcher-electron
+#!/usr/bin/env bash
+
+# choose .deb
+gh install balena-io/etcher
