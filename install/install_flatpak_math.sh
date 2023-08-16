2c8be33d4cd1165a8af54c8a93849b8d116ce7be
Bradley Bossard <bradleybossard@gmail.com>
2023-08-16 10:14:24 -0700

    add cantor to app installs

--- install/install_flatpak_math.sh
+++ install/install_flatpak_math.sh
@@ -4,6 +4,9 @@ flatpak install flathub org.jupyter.JupyterLab
 
 flatpak install --noninteractive flathub org.geogebra.GeoGebra
 
+# KDE Frontend to Mathematical Software
+flatpak install flathub org.kde.cantor
+
 # fractals
 flatpak install --noninteractive flathub io.github.xaos_project.XaoS
 flatpak install --noninteractive flathub com.github.buddhi1980.mandelbulber2
