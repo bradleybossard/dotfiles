3a262e4ce63d1cb9fe1f062715eafd4b08e9bdef
Bradley Bossard <bradleybossard@gmail.com>
2023-10-03 07:20:35 -0700

    adding more flatpaks

--- install/install_flatpak_emulators.sh
+++ install/install_flatpak_emulators.sh
@@ -2,3 +2,6 @@
 
 flatpak install --noninteractive flathub org.ryujinx.Ryujinx
 flatpak install --noninteractive flathub org.yuzu_emu.yuzu
+
+# wii u
+flatpak install --noninteractive flathub info.cemu.Cemu
