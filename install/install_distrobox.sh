2932fd6d4b8bb1c6526f997825b1d5b4a2684892
Bradley Bossard <bradleybossard@gmail.com>
2024-03-31 08:11:12 -0700

    adding tips for distrobox-export

--- install/install_distrobox.sh
+++ install/install_distrobox.sh
@@ -3,3 +3,9 @@
 sudo add-apt-repository --yes ppa:michel-slm/distrobox
 sudo apt update
 sudo apt install --yes distrobox
+
+# distrobox-export --app brave-browser --export-label brave-<whatever>
+
+# to edit distrobox-export entries
+# ll ~/.local/share/applications
+# and find corresponding entry and edit with vi
