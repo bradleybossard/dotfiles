6cea1dbadff4465dbb76c7ba2b898fd2dd4064c8
Bradley Bossard <bradleybossard@gmail.com>
2025-02-15 15:52:57 -0800

    add windsurf install script

---
+++ install/install_windsurf.sh
@@ -0,0 +1,8 @@
+curl -fsSL "https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/windsurf.gpg" | \
+	sudo gpg --dearmor -o /usr/share/keyrings/windsurf-stable-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/windsurf-stable-archive-keyring.gpg arch=amd64] https://windsurf-stable.codeiumdata.com/wVxQEIWkwPUEAGf3/apt stable main" | \
+	sudo tee /etc/apt/sources.list.d/windsurf.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get upgrade --yes windsurf
