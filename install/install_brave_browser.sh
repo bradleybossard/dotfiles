6e2ef46a916c1955f7fe3ff168de2699342c9f84
Bradley Bossard <bradleybossard@gmail.com>
2025-04-19 12:17:46 -0700

    use omnibus installer for brave

--- install/install_brave_browser.sh
+++ install/install_brave_browser.sh
@@ -1,9 +1,9 @@
-sudo apt install --yes apt-transport-https curl
+# omnibus linux installer
+curl -fsS https://dl.brave.com/install.sh | sh
 
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-sudo apt update
-
-sudo apt install --yes brave-browser
+# ubuntu specific
+#sudo apt install --yes apt-transport-https curl
+#sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+#echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+#sudo apt update
+#sudo apt install --yes brave-browser
